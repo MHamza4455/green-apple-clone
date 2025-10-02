33e3e1eb2dad7d1bf986b224677f076105100346
MHamza4455 <hamzabahir541@gmail.com>
2025-10-02 12:11:32 +0500

    added whatsapp button on screen bottom

--- src/components/StandardLayout.tsx
+++ src/components/StandardLayout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { WhatsAppFloat } from "./WhatsAppFloat";
 
 interface StandardLayoutProps {
   children: React.ReactNode;
 }
 
 export const StandardLayout: React.FC<StandardLayoutProps> = ({ children }) => {
-  return <div className="min-h-screen bg-white">{children}</div>;
+  return (
+    <div className="min-h-screen bg-white">
+      {children}
+      <WhatsAppFloat phoneNumber="+971547861293" />
+    </div>
+  );
 };
 
 export default StandardLayout;
